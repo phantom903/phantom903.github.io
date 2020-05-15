a9f11ec68602a9b48718d322f16603bc3a4fa0c5
Dave Ponting <pontingdavid.81@gmail.com>
2020-05-15 12:34:45 +0100

    rewrite

--- main.js
+++ main.js
@@ -11,28 +11,32 @@ function loadJson() {
 
 function populateCards(jsonData) {
   //var jsonData = JSON.parse(json);
-  Object.entries(jsonData).forEach(element => {
+  for (var element of Object.entries(jsonData)) {
     console.log(element);
-    var cardHeader = document.createElement("h4");
-    cardHeader.setAttribute("class", "card-title");
-    cardHeader.innerHTML = element.title;
+  }
+  // Object.entries(jsonData).forEach(element => {
+  //   console.log(element);
+  //   var cardHeader = document.createElement("h4");
+  //   cardHeader.setAttribute("class", "card-title");
+  //   cardHeader.innerHTML = element.title;
 
-    var cardContent = document.createElement("p");
-    cardContent.setAttribute("class", "card-text");
-    element.links.forEach(link => {
-      var cardLink = document.createElement("a");
-      cardLink.setAttribute("href", link.url);
-      cardLink.innerHTML = link.name;
-      cardContent.appendChild(cardLink);
-    })
+  //   var cardContent = document.createElement("p");
+  //   cardContent.setAttribute("class", "card-text");
+  //   element.links.forEach(link => {
+  //     var cardLink = document.createElement("a");
+  //     cardLink.setAttribute("href", link.url);
+  //     cardLink.innerHTML = link.name;
+  //     cardContent.appendChild(cardLink);
+  //   })
 
-    var cardBlock = document.createElement("div");
-    cardBlock.setAttribute("class", "card-block");
-    cardBlock.appendChild(cardHeader);
-    cardBlock.appendChild(cardContent);
+  //   var cardBlock = document.createElement("div");
+  //   cardBlock.setAttribute("class", "card-block");
+  //   cardBlock.appendChild(cardHeader);
+  //   cardBlock.appendChild(cardContent);
     
-    var newCard = document.createElement("div");
-    newCard.setAttribute("class", "card text-center");
-    newCard.appendChild(cardBlock);
-  });
+  //   var newCard = document.createElement("div");
+  //   newCard.setAttribute("class", "card text-center");
+  //   newCard.appendChild(cardBlock);
+  // });
+
 }
