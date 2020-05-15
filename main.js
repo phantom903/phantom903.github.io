95ea8d96f58a048e4efd4a49b8499fef5f821770
Dave Ponting <pontingdavid.81@gmail.com>
2020-05-15 12:09:15 +0100

    debug

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ function loadJson() {
   xmlRequest.send();
   xmlRequest.onload = function () {
     json = request.response;
+    console.log(request.response);
   }
-  console.log(json);
   return JSON.parse(json);
 }  
 
